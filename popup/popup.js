2270c4f7dc853203cb03aa7b223167a3594b78b6
MarisKori <maris.kori@gmail.com>
2018-10-10 23:28:22 +0300

    0.18

--- popup/popup.js
+++ popup/popup.js
@@ -5,26 +5,22 @@ function add(info) {
 	html += info + "<br>\n";
 }
 
-let save_ip; //={}
-function show_ip_list() {
-	/*let cnt=0;
-	for(let ip in save_ip.ip) {
-		cnt++;
-		if (cnt>1)break;
-	}*/
+
+function get_ip_list() {
 	//background.console.log('show_ip_list');
 	let hint = background.icon_hint;
 	let html = '';
-	let is_proxy;
-	for(let ip in save_ip.ip) {
+	let proxy_index;
+	for(let ip in hint.ip_arr) {
+		if (ip == hint.hostname) continue;
 		let line = ip;
-		let status = save_ip.ip[ip];
+		let status = hint.ip_arr[ip];
 		if (status == 1) line = '<font color="#ff8e00">'+line+'</font>';
 		else if (status == 2) line = '<font color="#ff0000">'+line+'</font>';
 		else if (status == 5) line = '<font color="#00bb00">'+line+'</font>';
 		if (background.known_proxy_ip[ip]) { //proxy may be blocked
 			line = '<font color="#0000ff">('+line+')</font>';
-			if (ip == hint.ip) is_proxy = background.known_proxy_ip[ip];
+			if (ip == hint.ip) proxy_index = background.known_proxy_ip[ip];
 		}
 		line = '<b>'+line+'</b>';
 		if (ip == hint.ip) {
@@ -32,35 +28,28 @@ function show_ip_list() {
 		}
 		html += line + '<br>';
 	}
-	if (is_proxy) {
-		let proxy_name = background.known_proxy_name[is_proxy] || 'Вы используете прокси';
+	if (proxy_index) {
+		let proxy_name = background.known_proxy_name[proxy_index] || 'Вы используете прокси';
 		html+=('<font color="#0000ff">'+proxy_name+'</font><br>');
 	}
-	document.getElementById("dns_ip").innerHTML = html;
+	return html;
 }
 
-function update_dns_records(rec) {
-	//background.console.log('update_dns_records',rec);
-	for(let ip in rec.ip) {
-		let status = rec.ip[ip];
-		if (save_ip.ip[ip]) continue;
-		save_ip.ip[ip] = status > 0 ? status : 5;
-	}
-	show_ip_list();
-}
 
-function update_site_status(is_up) {
+function update_site_status(is_up_rec) {
+	let hint = background.icon_hint;
+	if (hint.real_domain != is_up_rec.hostname) return;
 	let check_site_result = document.getElementById('check_site_result');
+	if (!check_site_result) return background.console.warn('No check_site_result!'); //jj
+	const is_up = is_up_rec.result;
 	if (is_up === "?")
-			check_site_result.innerHTML = ' <img src="/images/unknown_16.png" alt="Невозможно проверить доступность сайта." title="Невозможно проверить доступность сайта.">';
-	else if (is_up) check_site_result.innerHTML = ' <img src="/images/ok.png" alt="Сайт доступен." title="Сайт доступен.">';
-	else check_site_result.innerHTML = ' <img src="/images/down.png" alt="Сайт лежит." title="Сайт лежит.">';
+		check_site_result.innerHTML = ' <img src="/images/unknown_16.png" alt="Невозможно проверить доступность сайта." title="Невозможно проверить доступность сайта.">';
+	else if (is_up===true) check_site_result.innerHTML = ' <img src="/images/ok.png" alt="Сайт доступен." title="Сайт доступен.">';
+	else if(is_up===false) check_site_result.innerHTML = ' <img src="/images/down.png" alt="Сайт лежит." title="Сайт лежит.">';
+	else check_site_result.innerHTML = ' [error]';
 }
 
-let update_dns_timer;
-let first_time=true; //omg!
 function popup_update() {
-	clearTimeout(update_dns_timer);
 	let hint = background.icon_hint;
 	//announcement
 	if (hint.replace_text) {
@@ -69,62 +58,37 @@ function popup_update() {
 	}
 	html = '';
 	//domain
-	if (hint.hostname && !(hint.ip_info && /^\d+\.\d+\.\d+\.\d+$/.test(hint.hostname))) {
+	if (hint.hostname) { // && !(hint.ip && /^\d+\.\d+\.\d+\.\d+$/.test(hint.hostname))) {
 		let is_up_status = '';
-		if (background.localStorage.check_site_is_online==1 && first_time) {
-			let is_up = background.check_if_is_online(hint.hostname, update_site_status); //async!
-			if (is_up !== undefined) {  //cached result
-				setTimeout(function(){ update_site_status(is_up); },0);
-			}
-			else {
-				is_up_status = ' <img src="/images/loading_16.gif">';
-			}
+		if (background.localStorage.check_site_is_online==1) {
+			is_up_status = ' <img src="/images/loading_16.gif">';
 		}
-		add("<b>" + hint.hostname + '<span id="check_site_result">'+is_up_status+'</span></b>');
+		add("<b><span"+(hint.domain_blocked?' class=red':'')+">"
+			+ hint.hostname + '</span><span id="check_site_result">'+is_up_status+'</span></b>');
 	}
 	//date when was blocked by RKN
 	if (hint.date) add("Дата блокировки: " + hint.date);
 	//comment
 	else if (hint.hostname) {
-		if (!(hint.ip && hint.ip_info && hint.ip_info.ip[hint.ip] == 2))
+		if (!(hint.is_ip_blocked > 0))
 			add("В реестре отсутствует.");
 	}
 	else if (hint.no_init) add('База данных не загружена.');
 	else add('Это вообще не сайт.');
 	if (hint.text) add(hint.text);
 	//info about ip
-	html+='<span id="dns_ip"></span>';
-	if (hint.ip_info) { //list of ips
-		let info = hint.ip_info;
-		save_ip = {ip:info.ip};
-		setTimeout(show_ip_list,0);
-		/*let is_red;
-		for (let ip in info.ip) {
-			if (ip == hint.ip && info.ip[ip] == 2) {
-				is_red = true;
-				break;
-			}
-		}*/
-		//if (info.is_error || is_red)
-	}
-	else save_ip = {ip:{}};
-	if (background.localStorage.use_httpdns == 1 && first_time) { //check dns records
-		//background.console.log('check dns');
-		update_dns_timer = setTimeout(function(){
-			background.getDNS(hint.hostname, update_dns_records);
-		},0); //next tick
-	}
+	html+=get_ip_list();
 	if (hint.reason) {
 		add('<b>Причина:</b>');
 		add("Гос. орган: <b>"+hint.reason.gos_organ+"</b>");
 		add("Постановление: <b>"+hint.reason.postanovlenie+"</b>");
 	}
-	if (hint.ip_info && hint.ip_info.reason[hint.ip]) {
-		let reason = hint.ip_info.reason[hint.ip];
+	if (hint.reason_ip) {
+		let reason = hint.reason_ip;
 		if (!hint.reason || hint.reason.postanovlenie != reason.postanovlenie ) {
 			add('<b>Блокировка по ip:</b>');
 			if (reason.mask) {
-				add("<font color=red><b>"+reason.mask+"</b></font>");
+				add("<font color=red>"+reason.mask+"</font>");
 			}
 			add("Гос. орган: <b>"+reason.gos_organ+"</b>");
 			add("Постановление: <b>"+reason.postanovlenie+"</b>");
@@ -135,7 +99,6 @@ function popup_update() {
 	else if (hint.is_whitelist_ip_local) add('<span class=whitelist>Локальный ip-адрес</span>');
 	else if (hint.permanently_blocked) add('<span class=permanently_blocked>Вечная блокировка</span>');
 	div_data.innerHTML = html;
-	first_time = false;
 }
 
 let div_data;
@@ -144,7 +107,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	div_data = document.getElementById('data');
 	background.popup_update_unsafe = popup_update; //will recieve updated data
 	popup_update(); //update now (initialize)
-	background.popup = window;
+	background.onPopupOpen(window);
 })
 
 
