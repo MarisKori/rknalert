0de72bb558e43a861ddf0f6b926ebdb81b5fe3da
Maris <maris.kori@gmail.com>
2019-05-21 08:53:53 +0300

    0.18.9

--- check.js
+++ check.js
@@ -11,14 +11,15 @@ function report(name,b) {
 function checkRuTracker() {
 	for(let e of d.head.querySelectorAll('link')) {
 		if (e.href.indexOf('//static.t-ru.org/') > -1) return report('rutracker',1);
+		if (e.title=="Поиск на RuTracker.org") return report('rutracker',0);
 	}
-	return report('rutracker',0);
 }
 
 function checkTitle(l) {
 	if (l.indexOf('rutracker.org') > -1 || l.match(/\brutracker\b/)) checkRuTracker();
 	else if (l.indexOf('зеркало rutor.info ') === 0) report('rutor',1);
 	else if (l.match(/(?:[^\w]|^)rutor\.org\b/) || l.match(/^rutor\b/)) report('rutor',0);
+	else if (l.indexOf(' :: nnm-club')> -1) report('nnmclub',0);
 	else {
 		for(let e of d.head.querySelectorAll('link')) {
 			if (e.rel == 'search' && e.title && e.title.toLowerCase().indexOf('rutracker.org') > -1)
